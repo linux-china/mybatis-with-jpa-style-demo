6ae8a16bbf62cb13a5ff3fc447eddc8c60680637
linux_china <linux_china@hotmail.com>
2018-03-20 18:18:25 +0800

    flyway V1: city

---
+++ src/test/resources/db/migration/V1__city.sql
@@ -0,0 +1,7 @@
+CREATE TABLE city (
+  id         INT PRIMARY KEY AUTO_INCREMENT,
+  name       VARCHAR(64),
+  state      VARCHAR(32),
+  country    VARCHAR(8),
+  created_at DATETIME
+) DEFAULT CHARSET = utf8;
