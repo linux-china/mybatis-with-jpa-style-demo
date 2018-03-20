5e337325f58e9fe3364692a9eb7000cddde67e15
linux_china <linux_china@hotmail.com>
2018-03-20 18:14:52 +0800

    spring boot app

---
+++ src/main/java/org/mvnsearch/mybatis/jpaStyle/MyBatisJpaDemoApplication.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.mybatis.jpaStyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisJpaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisJpaDemoApplication.class, args);
+    }
+}
